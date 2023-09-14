95763484eae7edd48ac4df2215ef48f1671eef12
Daccarett Anthony <80802526+AnthonyDac@users.noreply.github.com>
2023-09-14 17:26:29 +0200

    mise en place de la page logement + maj router

--- kasa/src/router/Routes.jsx
+++ kasa/src/router/Routes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/Homepage/HomePage';
 import AboutPage from '../pages/About/AboutPage';
 import NotFoundPage from '../pages/404/404Page';
+import LogementPage from '../pages/Logement/LogementPage';
 
 const Routers = () => {
     return (
@@ -11,6 +12,7 @@ const Routers = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
+                <Route path="/logement/:id" element={<LogementPage />} />
                 <Route path="/404" element={<NotFoundPage />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
