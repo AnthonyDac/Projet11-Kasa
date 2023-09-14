81c6c2f28bef0c245d0f179963cfff908cc9a9ae
Daccarett Anthony <80802526+AnthonyDac@users.noreply.github.com>
2023-09-14 15:45:48 +0200

    création de la page 404 + ajout au routeur

--- kasa/src/router/Routes.jsx
+++ kasa/src/router/Routes.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/Homepage/HomePage';
 import AboutPage from '../pages/About/AboutPage';
-
+import NotFoundPage from '../pages/404/404Page';
 
 const Routers = () => {
     return (
@@ -11,6 +11,8 @@ const Routers = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
+                <Route path="/404" element={<NotFoundPage />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
     );
